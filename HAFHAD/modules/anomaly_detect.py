74a04a3325e234d7634b7916107ec52d7bd58248
Unknown <gui.aria1@gmail.com>
2018-03-22 15:33:09 +0700

    Add comment

--- HAFHAD/modules/anomaly_detect.py
+++ HAFHAD/modules/anomaly_detect.py
@@ -2,24 +2,24 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
-import json
 from statistics import stdev
 from statistics import mean
 from pprint import pprint
 from tts import tts
 
 #creditial and initial
-
-
 cred = credentials.Certificate("sa_key/SA_key.json")
 firebase_admin.initialize_app(cred ,{
     'databaseURL' : 'https://dashboard-2f5e4.firebaseio.com/'
 })
 
+# get data from firebase
+
 ref = db.reference('/')
 ref1 = ref.get()
 element = ref1.get('Days')
 
+# check data
 size = 8
 if len(element) < 8 :
     print("Need more data")
@@ -30,6 +30,8 @@ else:
     list_mean = mean(element)
     anomaly = list_mean + 2*(list_sd)
     print("Mean + 2 SD = ",anomaly)
+
+# Alert
 
     if last_use > anomaly:
         tts("Alert Use more than Usaul")
