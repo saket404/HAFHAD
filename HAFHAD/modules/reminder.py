aba6b3b728fada9ad159034c0bee3e6b45884b82
saket404 <saketdecade17@gmail.com>
2018-03-25 02:57:07 -0700

    Fixed Google Calendar Bug

--- HAFHAD/modules/reminder.py
+++ HAFHAD/modules/reminder.py
@@ -68,11 +68,26 @@ def askDateTime():
         
         try:    
             
-            if("." in text or ":" in text):
+            if(":" in text):
                 time = text1[0] +":"+text1[2]
+            elif("." in text):
+                time = text1[0].replace(".",":")
             else:
                 time = text1[0] +":"+ text1[4]
             
+        
+        
+            if("วันนี้" in date):
+                tz = pytz.timezone(('Asia/Bangkok'))
+                d = datetime.datetime.now(tz=tz)
+                date = d.date()
+                
+            if("พรุ่งนี้" in date):
+                one_day = datetime.timedelta(days=1)
+                tz = pytz.timezone('Asia/Bangkok')
+                d = datetime.datetime.now(tz=tz) + one_day
+                date = d.date()
+        
             
             inputDate = date+","+time
         
@@ -104,6 +119,51 @@ def askDateTime():
 
 
 def addEvent():
+
+    # Create a flow object. This object holds the client_id, client_secret, and
+# scope. It assists with OAuth 2.0 steps to get user authorization and
+# credentials.
+
+    flow = OAuth2WebServerFlow(client_id, client_secret, scope)
+
+
+# Create a Storage object. This object holds the credentials that your
+# application needs to authorize access to the user's data. The name of the
+# credentials file is provided. If the file does not exist, it is
+# created. This object can only hold credentials for a single user, so
+# as-written, this script can only handle a single user.
+    storage = Storage('credentials.dat')
+
+# The get() function returns the credentials for the Storage object. If no
+# credentials were found, None is returned.
+    credentials = storage.get()
+
+# If no credentials are found or the credentials are invalid due to
+# expiration, new credentials need to be obtained from the authorization
+# server. The oauth2client.tools.run_flow() function attempts to open an
+# authorization server page in your default web browser. The server
+# asks the user to grant your application access to the user's data.
+# If the user grants access, the run_flow() function returns new credentials.
+# The new credentials are also stored in the supplied Storage object,
+# which updates the credentials.dat file.
+    if credentials is None or credentials.invalid:
+        credentials = run_flow(flow, storage)
+
+# Create an httplib2.Http object to handle our HTTP requests, and authorize it
+# using the credentials.authorize() function.
+    http = httplib2.Http()
+
+    http = credentials.authorize(http)
+
+# The apiclient.discovery.build() function returns an instance of an API service
+# object can be used to make API calls. The object is constructed with
+# methods specific to the calendar API. The arguments provided are:
+#   name of the API ('calendar')
+#   version of the API you are using ('v3')
+#   authorized httplib2.Http() object that can be used for API calls
+    service = build('calendar', 'v3', http=http)
+    
+    
 
     try:
         event = {
@@ -148,7 +208,7 @@ def addEvent():
             
             flag = 0
             while flag == 0:
-                tts("โอเคไหม่ค่ะ")
+                tts("ถูกต้องหรือไม่ค่ะ")
                 try:
                     inputsay = stt()
                     if(inputsay == "ไม่เข้าใจที่พูดออกมาค่ะ"):
@@ -158,16 +218,16 @@ def addEvent():
                     tts("อินเทอร์เน็ตมีปัญหาค่ะ")
                     continue
                 
-            if any(["โอเค" in inputsay,"คับ" in inputsay,"ค่ะ" in inputsay,"OK" in inputsay]):
+            if any(["โอเค" in inputsay,"ครับ" in inputsay,"ค่ะ" in inputsay,"ถูก" in inputsay]):
                 service.events().insert(calendarId='primary',sendNotifications = True, body = event).execute()
-                break
+                
             
             warning = 1
                 
         
         
     except Exception as e:
-        tts("มีปัญหาในการต่อปฏิทิน Google ค่ะ")
+        tts("มีปัญหาในการต่อปฏิทินค่ะ")
         
     except KeyError:
         tts("มีปัญหาในการต่อปฏิทิน Google ค่ะ")
