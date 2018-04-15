bcab27da73f76b1f0a57b8a22162d7dea24599a4
Unknown <gui.aria1@gmail.com>
2018-04-16 00:25:06 +0700

    Anomaly Detection update to Google Cloud SQL

--- HAFHAD/modules/anomaly_detect.py
+++ HAFHAD/modules/anomaly_detect.py
@@ -1,47 +1,112 @@
 # Import database module.
 import firebase_admin
+import mysql.connector
 from firebase_admin import credentials
+
 from firebase_admin import db
+
 from statistics import stdev
+
 from statistics import mean
+
 from pprint import pprint
-from tts import tts
+from cloudConnect import gcloudConnect
+from cloudConnect import insertCloud
+
+from datetime import date, datetime, timedelta
+
+
+#from tts import tts
+
+
 
 def anomaly_detection():
+
     
+
     #creditial and initial
+
     cred = credentials.Certificate("sa_key/SA_key.json")
+
     firebase_admin.initialize_app(cred ,{
+
         'databaseURL' : 'https://dashboard-2f5e4.firebaseio.com/'
+
     })
+
+
 
     # get data from firebase
 
+
+
     ref = db.reference('/')
+
     ref1 = ref.get()
+
     element = ref1.get('Days')
 
+
+
     # check data
+
     size = 8
+
     if len(element) < 8 :
+
         print("Need more data")
+
     else:
+
         last_use = element.pop()
+
         element = element[-7:]
+
         list_sd = stdev(element)
+
         list_mean = mean(element)
+
         anomaly = list_mean + 2*(list_sd)
+
         print("Mean + 2 SD = ",anomaly)
 
-    # Alert
+        add_noti = ("INSERT INTO notification"
+					"(userId,content,type)"
+					"VALUE (%s,%s,%s)")
 
         if last_use > anomaly:
-            tts("Alert Use more than Usaul")
+			#print('Alert Use more than Usaul')
+            noti =('1','Alert Use more than Usaul','warning')
+
+        #    tts("Alert Use more than Usaul")
+
         elif last_use < anomaly:
-            tts("Alert Use better than Usual")
+			#print('Alert Use better than Usual')
+            noti =('1','Alert Use better than Usual','info')
+
+           # tts("Alert Use better than Usual")
+        insertCloud(add_noti,noti)
+
+
+
+
+if __name__ == '__main__':
+
+    anomaly_detection()
+
+    
+
+
 
             
+
             
+
+
+
+
+
+
 
 
 
---
+++ HAFHAD/modules/cloudConnect.py
@@ -0,0 +1,47 @@
+import mysql.connector
+from mysql.connector import errorcode
+config = {
+  'user': 'test',
+  'password': '1234',
+  'host': '35.189.145.19',
+  'database': 'User',
+  'raise_on_warnings': True,
+}
+def gcloudConnect():
+	
+	cnx = mysql.connector.connect(**config)
+	cursor = cnx.cursor()
+	print("Connected to Database")
+
+
+	#for name, ddl in TABLES.items():
+	#	try:
+	#		print("Creating table {}:".format(name),end='')
+	#		cursor.execute(ddl)
+	#	except mysql.connector.Error as err:
+	#		if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+	#			print("Already exists.")
+	#		else:
+	#			print(err.msg)
+	#	else:
+	#		print("OK")
+	return cnx
+
+def insertCloud(structure,data):
+	cnx = mysql.connector.connect(**config)
+	cursor = cnx.cursor()
+	print("Connected to Database")
+	cursor.execute(structure,data)
+	cnx.commit()
+	cursor.close()
+	cnx.close
+	print("Added data to cloud successfuly")
+	
+	return 0
+	
+	
+
+if __name__ == '__main__':
+    gcloudConnect()
+    
+
